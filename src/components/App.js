5489a3a2722127e77e0610c03318d6186e0a8f31
Selma Datagirl <datagirlcmr@gmail.com>
2020-05-08 13:14:11 -0500

    refactored the css import

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-import '../App.css';
 
 class App extends React.Component {
   constructor(props) {
