855fb441b2134950504b72107a6e98ea2d71c6a9
Selma Datagirl <datagirlcmr@gmail.com>
2020-05-12 05:29:08 -0500

    implemented the operations logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+// eslint-disable-next-line no-unused-vars
+import Calculate from '../logic/calculate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
     };
   }
 
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,20 @@
-// function Calculate(calculatorData, buttonName) {
-//   let { total, next, operation } = calculatorData;
+import operate from './operate';
 
-// }
+function Calculate(calculatorData, buttonName) {
+  // eslint-disable-next-line prefer-const
+  let { total, next, operation } = calculatorData;
+  const dataNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const dataOperations = ['+', '-', 'x', '÷', '%'];
 
-// export default Calculate;
+  if (buttonName === '+/-') {
+    total *= -1;
+  } else if (dataNumbers.includes(buttonName)) {
+    total += buttonName;
+  } else if (dataOperations.includes(buttonName)) {
+    total = operate(total, next, operation);
+  }
+
+  return { total, next, operation };
+}
+
+export default Calculate;
